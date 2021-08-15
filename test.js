b2a2cad2734c5ba36fb9cb61874b7e032cb3c73d
Duncan Ndegwa <ndegwaduncan@icloud.com>
2021-08-15 19:52:50 -0400

    testing all functions on inputs with X, + and - signs

--- test.js
+++ test.js
@@ -1,57 +1,257 @@
-// const itemsToProcess = "6,-,1,+,3"; // before the splitting;
-const itemsToProcess = "6,+,1,-,3"; // before the splitting;
+//     getpreviousAndNextNumbers of an operation
+const getpreviousAndNextNumbers = (currentMethod, bodmasNumber) => {
+  const currentMethodPosition = bodmasNumber.indexOf(currentMethod);
+  // console.log("currentMethodPosition is " + currentMethodPosition);
+
+  if (
+    bodmasNumber.length == 5 &&
+    bodmasNumber.includes("+") &&
+    bodmasNumber.includes("-") &&
+    bodmasNumber.indexOf("+") > bodmasNumber.indexOf("-")
+  ) {
+    const previousNumber = bodmasNumber[0];
+    const nextNumber = bodmasNumber.slice(
+      currentMethodPosition + 1,
+      bodmasNumber.length
+    );
+    const CollectedNumbers = previousNumber + "," + nextNumber;
+    return CollectedNumbers;
+    //   }
+  } else {
+    // otherwise obtain index as usual
+    // console.log(bodmasNumber + " has no regex");
+    // get index for previous number
+    const previousNumberIndex = currentMethodPosition - 1;
+    // get previous number
+    const previousNumber = bodmasNumber[previousNumberIndex];
+    // get index for next number
+    const nextNumberIndex = currentMethodPosition + 1;
+    // get next number
+    const nextNumber = bodmasNumber[nextNumberIndex];
+    // const nextNumber = '';
+
+    // combine previous and next numbers
+    const CollectedNumbers = previousNumber + "," + nextNumber;
+    return CollectedNumbers;
+  }
+};
+
+let inputtPreviousAndNextNumbers;
+let inputmethod;
+let inputFirstNumber;
+let inputSecondNumber;
+let collectedMethods;
+
+//     perform /, *, + or - operation
+const performOperation = (inputmethod, inputFirstNumber, inputSecondNumber) => {
+  if ("/".valueOf() == inputmethod.valueOf()) {
+    const product = Number(inputFirstNumber) / Number(inputSecondNumber);
+    return product;
+  } else if ("X".valueOf() == inputmethod.valueOf()) {
+    const product = Number(inputFirstNumber) * Number(inputSecondNumber);
+    return product;
+  } else if ("+".valueOf() == inputmethod.valueOf()) {
+    const product = Number(inputFirstNumber) + Number(inputSecondNumber);
+    return product;
+  } else if ("-".valueOf() == inputmethod.valueOf()) {
+    const product = Number(inputFirstNumber) - Number(inputSecondNumber);
+    return product;
+  }
+};
+
+//  get previous and next items in the input after bodmas
+let currentOperation;
+let itemsInOperation;
+
+const getRemainingItems = (currentOperation, itemsInOperation) => {
+  const currentOperationPosition = itemsInOperation.indexOf(currentOperation);
+
+  if (currentOperationPosition == 1 && inputMethods.length <= 3) {
+    const previousItems = "";
+    const nextItems = "";
+    const remainingItems = previousItems + nextItems;
+    return remainingItems;
+  } else if (inputMethods.length > 3) {
+    if (currentOperationPosition == 1) {
+      const previousItems = "";
+      const nextItems = itemsInOperation.slice(
+        itemsInOperation.indexOf(currentOperation) + 2
+      );
+      const remainingItems = previousItems + "," + nextItems;
+      return remainingItems;
+    } else if (currentOperationPosition == itemsInOperation.length - 2) {
+      const previousItems = itemsInOperation.slice(
+        0,
+        itemsInOperation.length - 3
+      );
+      const nextItems = "";
+      const remainingItems = previousItems + "," + nextItems;
+      return remainingItems;
+    } else {
+      const previousmethodPosition = currentOperationPosition - 2;
+      const nextMethodPosition = currentOperationPosition + 2;
+      const previousItems = itemsInOperation.slice(
+        0,
+        previousmethodPosition + 1
+      );
+      const nextItems = itemsInOperation.slice(nextMethodPosition);
+      const remainingItems = previousItems + "," + nextItems;
+      return remainingItems;
+    }
+  }
+};
+
+let remainingItems;
+let operationProduct;
+
+//     join product and remaining items
+const addProductToRemainingItems = (
+  currentOperation,
+  itemsInOperation,
+  remainingItems,
+  operationProduct
+) => {
+  const currentOperationPosition = itemsInOperation.indexOf(currentOperation);
+  if (currentOperationPosition == 1) {
+    const regeneratedItems = operationProduct + remainingItems;
+    return regeneratedItems;
+  } else if (currentOperationPosition == itemsInOperation.length - 2) {
+    const regeneratedItems = remainingItems + operationProduct;
+    return regeneratedItems;
+  } else {
+    const previousmethodPosition = currentOperationPosition - 1;
+    const nextMethodPosition = currentOperationPosition + 2;
+    const previousItems = itemsInOperation.slice(0, previousmethodPosition);
+    const nextItems = itemsInOperation.slice(nextMethodPosition);
+    const regeneratedItems =
+      previousItems + "," + operationProduct + "," + nextItems;
+    return regeneratedItems;
+  }
+};
+
+const itemsToProcess = "3,+,5,X,6,-,2"; // before the splitting;
+const inputMethods = ",+,,X,,-,".split(",");
+// console.log(inputMethods);
+// console.log(inputMethods.length);
+
 // console.log(itemsToProcess)
 const itemsToProcessSplitted = itemsToProcess.split(","); //after splitting;
-console.log(itemsToProcessSplitted);
+// console.log(itemsToProcessSplitted);
 
-// const regex = /^\d,-,\d,\+,\d$/g; // what works
-const regex = /^(?=\d,-,)\d,\+,\d$/g; // what works
-// const regex = /(\d,)-{1},(\d,)\+{1},\d/g; // what works
-// const regex = /\d,-{1},(?=\d,\+{1},\d)/g; // what works
-// const regex1 = /\d,-{1},(?!\d,\+{1},\d)/g; // what works
-// const regex2 = /(?=\d,-{1},)\d,\+{1},\d/g; // what works
-// const regex = /^(\d(?=-)\d(?=\+)\d)$/;
-// const regex = /^(\d(?!-)\d(?!\+)\d)$/;
-// const regex = /^(\d(?!,\+),-,\d(?!,-),\+,\d)$/g;
-// const regex = /^(\d,-),\d,(\+,\d)$/g;
-const bodmasNumber = itemsToProcessSplitted;
-const currentMethod = "+";
-const currentMethodPosition = bodmasNumber.indexOf(currentMethod);
+const InpuItemsToProcess = itemsToProcessSplitted;
+console.log("InpuItemsToProcess " + InpuItemsToProcess);
+// console.log(bodmasNumber.length);
+// const currentMethod = "X";
+// const currentMethodPosition = bodmasNumber.indexOf(currentMethod);
 
-console.log(regex.test(bodmasNumber) != -1);
-// console.log(regex1.test(bodmasNumber) != -1 && regex2.test(bodmasNumber) != -1);
+if (
+  inputMethods.includes("X") &&
+  inputMethods.includes("+") &&
+  inputMethods.includes("-") &&
+  inputMethods.length == 7
+) {
+  //  multiplication
+  const multiplicationPreviousAndNextNumbers = getpreviousAndNextNumbers(
+    "X",
+    InpuItemsToProcess
+  );
+  console.log(
+    "multiplicationPreviousAndNextNumbers " +
+      multiplicationPreviousAndNextNumbers
+  );
+  const multiplicationFirstNumber =
+    multiplicationPreviousAndNextNumbers.split(",")[0];
+  const multiplicationSecondNumber =
+    multiplicationPreviousAndNextNumbers.split(",")[1];
+  const multiplicationProduct = performOperation(
+    "X",
+    multiplicationFirstNumber,
+    multiplicationSecondNumber
+  );
+  console.log("multiplicationProduct " + multiplicationProduct);
 
-// // if (regex.test(bodmasNumber) != -1) {
-// if (regex.testExact(bodmasNumber) != -1) {
-//   // get index of + and - if available
-//   console.log(bodmasNumber + " has regex");
-//   const plusSignIndex = bodmasNumber.indexOf("+");
-//   const minusSignIndex = bodmasNumber.indexOf("-");
-//   if (plusSignIndex > minusSignIndex && currentMethod == "+") {
-//     const previousNumber = bodmasNumber[0];
-//     const nextNumber = bodmasNumber.slice(
-//       currentMethodPosition + 1,
-//       bodmasNumber.length
-//     );
-//     const CollectedNumbers = previousNumber + "," + nextNumber;
-//     // return CollectedNumbers;
-//     console.log(CollectedNumbers);
-//   }
-// } else {
-//   // otherwise obtain index as usual
-//   console.log(bodmasNumber + " has no regex");
-//   // get index for previous number
-//   const previousNumberIndex = currentMethodPosition - 1;
-//   // get previous number
-//   const previousNumber = bodmasNumber[previousNumberIndex];
-//   // get index for next number
-//   const nextNumberIndex = currentMethodPosition + 1;
-//   // get next number
-//   const nextNumber = bodmasNumber[nextNumberIndex];
-//   // const nextNumber = '';
+  // addition
+  const multiplicationOtherRemainingItems = getRemainingItems(
+    "X",
+    InpuItemsToProcess
+  );
 
-//   // combine previous and next numbers
-//   const CollectedNumbers = previousNumber + "," + nextNumber;
-//   // return CollectedNumbers;
-//   console.log(CollectedNumbers);
-// }
+  console.log(
+    "multiplicationOtherRemainingItems " + multiplicationOtherRemainingItems
+  );
+
+  const multiplicationProductAddedToItems = addProductToRemainingItems(
+    "X",
+    InpuItemsToProcess,
+    multiplicationOtherRemainingItems,
+    multiplicationProduct
+  );
+
+  console.log(
+    "multiplicationProductAddedToItems " + multiplicationProductAddedToItems
+  );
+
+  const addittionPreviousAndNextNumbers = getpreviousAndNextNumbers(
+    "+",
+    multiplicationProductAddedToItems.split(",")
+  );
+
+  console.log(
+    "addittionPreviousAndNextNumbers " + addittionPreviousAndNextNumbers
+  );
+  // setAnswer((answer) => answer + addittionPreviousAndNextNumbers);
+
+  const additionFirstNumber = addittionPreviousAndNextNumbers.split(",")[0];
+  const additionSecondNumber = addittionPreviousAndNextNumbers.split(",")[1];
+  // setAnswer(
+  //   (answer) => answer + additionFirstNumber + "," + additionSecondNumber
+  // );
+  const additionProduct = performOperation(
+    "+",
+    additionFirstNumber,
+    additionSecondNumber
+  );
+
+  console.log("additionProduct " + additionProduct);
+
+  // subtraction
+  const additionOtherRemainingItems = getRemainingItems(
+    "+",
+    multiplicationProductAddedToItems.split(",")
+  );
+
+  console.log("additionOtherRemainingItems  " + additionOtherRemainingItems);
+
+  const additionProductAddedToItems = addProductToRemainingItems(
+    "+",
+    multiplicationProductAddedToItems.split(","),
+    additionOtherRemainingItems,
+    additionProduct
+  );
+
+  console.log("additionProductAddedToItems " + additionProductAddedToItems);
+  const subtractionPreviousAndNextNumbers = getpreviousAndNextNumbers(
+    "-",
+    additionProductAddedToItems.split(",")
+  );
+
+  console.log(
+    "subtractionPreviousAndNextNumbers " + subtractionPreviousAndNextNumbers
+  );
+
+  const subtractionFirstNumber =
+    subtractionPreviousAndNextNumbers.split(",")[0];
+  const subtractionSecondNumber =
+    subtractionPreviousAndNextNumbers.split(",")[1];
+  const subtractionProduct = performOperation(
+    "-",
+    subtractionFirstNumber,
+    subtractionSecondNumber
+  );
+
+  console.log("subtractionProduct " + subtractionProduct);
+  // // setAnswer(answer => answer + '=' + subtractionProduct);
+  // setAnswer((answer) => answer + subtractionProduct);
+}
+
+// const itemsToProcess = "6,-,1,+,3"; // before the splitting;
